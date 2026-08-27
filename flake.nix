d2c92348899a0aae71360de6669c8638ab25e0a9
Nyramu <nyramu@noreply.codeberg.org>
2026-08-28 00:31:15 +0200

    feat: add flake

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Cemu (Wii U emulator) built from the latest git main";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    import-tree.url = "github:vic/import-tree";
+  };
+
+  outputs =
+    inputs@{ flake-parts, import-tree, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } (import-tree ./modules);
+
+  nixConfig = {
+    extra-substituters = [ "https://cemu-nix.cachix.org" ];
+    extra-trusted-public-keys = [
+      "cemu-nix.cachix.org-1:T/ZzErp7a/kDpY5U8FGtJPUcE9uGvJTCp2WrpidM2eE="
+    ];
+  };
+}
